ce67662db24120acc5c9aef113e9761d1a37e966
Sami-Shafi <freelancersami123@gmail.com>
2025-06-17 05:06:29 +0600

    Linear Time Complexity

--- main.cpp
+++ main.cpp
@@ -3,9 +3,23 @@ using namespace std;
 
 int main ()
 {
-    int n;
-    cin >> n;
+    int n; // O(1)
+    cin >> n; // O(1)
 
-    cout << n << endl;
-    return 0;
+    for (int i = 1; i <= n; i+=2) // O(n/2) -> O(n)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl; // O(1)
+
+    for (int i = 1; i <= n; i++) // O(n)
+    {
+        cout << i << " ";
+    }
+
+    return 0; // O(1)
+
+    // linear Time Complexity: Highest complexity without any constants as coefficient or adders.
+    // Ans: O(n)
 }
