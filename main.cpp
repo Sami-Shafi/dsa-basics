1dfa71cd0524eeff969d363c733e0895afaad455
Sami-Shafi <freelancersami123@gmail.com>
2025-06-19 01:20:36 +0600

    Logarithmic Complexity

--- main.cpp
+++ main.cpp
@@ -6,20 +6,19 @@ int main ()
     int n; // O(1)
     cin >> n; // O(1)
 
-    for (int i = 1; i <= n; i+=2) // O(n/2) -> O(n)
+    for (int i = 1; i <= n; i*=2) // O(log2>n) -> O(log(n))
     {
         cout << i << " ";
     }
 
     cout << endl; // O(1)
 
-    for (int i = 1; i <= n; i++) // O(n)
+    for (int i = n; i >= 1; i/=3) // O(log3>n) -> O(log(n))
     {
         cout << i << " ";
     }
 
     return 0; // O(1)
 
-    // linear Time Complexity: Highest complexity without any constants as coefficient or adders.
-    // Ans: O(n)
+    // Logarithmic Complexity: when the increment/decrement has multiplication or division
 }
