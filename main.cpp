05ff9cee3093b208b0d97956e74d2ee097c26349
Sami-Shafi <freelancersami123@gmail.com>
2025-06-19 02:04:05 +0600

    Square Root  Complexity

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@ using namespace std;
 
 int main ()
 {
-    int n, m; // O(1)
-    cin >> n >> m; // O(1)
+    int n; // O(1)
+    cin >> n; // O(1)
 
-    for (int i = 0; i < n; i++) { // O(n)
-        for (int j = 0; j < m; j++) // O(m)
-        {
-            cout << "Hello" << endl;
-        }
-        
+    // Problem: Find all divisors of 36
+    // Ans: 1, 2, 3, 4, 6, 9, 12, 18, 36
+    // Hint: here we don't need to loop till 36.. we can still solve the problem by looping till sqrt(36) -> 6
+
+    for (int i = 1; i <= sqrt(n); i++) // O(sqrt(n))
+    {
+        if (n%i!=0) continue;
+        cout << i << " " << (n/i) << " ";
     }
 
     return 0; // O(1)
 
-    // Quadratic Complexity: Usually happens in nested loops.. two Orders will get multiplied with each other.
-    // Ans: O(n*m)
+    // Sqrt: sometimes we can achieve lower time consumption by looping through square roots of numbers;
+    // Ans: O(sqrt(n))
 }
