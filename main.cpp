82c34ca206617630e40be2c82b1c9f0d6f4fa940
Sami-Shafi <freelancersami123@gmail.com>
2025-06-30 04:36:25 +0600

    Linked List

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node {
+
+    public:
+        int val;
+        Node* next;
+
+    
+
+};
+
 int main ()
 {
-    
+    Node a, b, c;
+    a.val = 10;
+    b.val = 20;
+    c.val = 30;
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    //testing the linked list
+    cout << a.val << " " << a.next->val << " " << ((a.next)->next)->val;
     
     return 0;
 }
