9c7f2f0453710dd50989892f7ed97567670a39b6
Sami-Shafi <freelancersami123@gmail.com>
2025-06-30 04:58:03 +0600

    Dynamic Linked List

--- main.cpp
+++ main.cpp
@@ -5,20 +5,26 @@ class Node {
 
     public:
         int val;
-        Node* next;
+        Node* nextNode;
 
     Node(int val) {
         this->val = val;
-        this->next = NULL;
+        this->nextNode = NULL;
     }
 
 };
 
 int main ()
 {
-    Node a(10), b(20), c(30);
-    a.next = &b;
-    b.next = &c;
+    // recap of dynamic: when you create anything wiht new keyword, it stores it into the heap memory and you have to assign it to a pointer var of same type. To access it, you can de-ref the pointer var.
+    Node* headNode = new Node(10);
+    Node* secondNode = new Node(20);
+    Node* thirdNode = new Node(30);
+
+    headNode->nextNode = secondNode;
+    secondNode->nextNode = thirdNode;
+
+    cout << headNode->val << " " << headNode->nextNode->val << " ";
     
     return 0;
 }
