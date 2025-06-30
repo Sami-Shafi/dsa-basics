f2c778d9ba5b06c8108274703c8c4b67a382ba9e
Sami-Shafi <freelancersami123@gmail.com>
2025-07-01 05:32:02 +0600

    Linked List Insert at Tail

--- main.cpp
+++ main.cpp
@@ -21,13 +21,41 @@ void insert_at_head(Node* &headNode, int val)
     headNode = newNode;
 }
 
+void insert_at_tail(Node* &headNode, int val)
+{
+    Node* newNode = new Node(val);
+
+    if (headNode == NULL) {
+        headNode = newNode;
+        return;
+    }
+
+    Node* tmp = headNode;
+    while (tmp->nextNode != NULL) {
+        tmp = tmp->nextNode;
+    }
+    tmp->nextNode = newNode;
+}
+
+void print_ll(Node* headNode)
+{
+    Node* tmp = headNode;
+    while (tmp != NULL) {
+        cout << tmp->val << endl;
+        tmp = tmp->nextNode;
+    }
+}
+
 int main ()
 {
-    Node* headNode = new Node(10);
-    Node* a = new Node(20);
-    Node* b = new Node(30);
+    Node* headNode = NULL;
 
+    insert_at_tail(headNode, 20);
+    insert_at_tail(headNode, 30);
     insert_at_head(headNode, 100);
+    insert_at_tail(headNode, 50);
+    insert_at_tail(headNode, 70);
+    print_ll(headNode);
     
     return 0;
 }
