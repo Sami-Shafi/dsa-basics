ab5f9c17ad88e40b08ceb2efd59c4e704c7997f2
Sami-Shafi <freelancersami123@gmail.com>
2025-07-20 11:54:02 +0600

    queries STL List

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 // You have a doubly linked list which is empty initially. Then you will be given Q queries. In each query you will be given two values X and V.
 
-// You need to insert the value V at index X. Assume that index starts from 0.
-// After that for each query you need to print the linked list from left to right and right to left.
-// If the index is invalid, then print "Invalid".
+// If X is 0 that means you will insert the value V to the head of the linked list.
+// If X is 1 then you will insert the value V to the tail of the linked list.
+// If X is 2 then you will delete the value Vth index of the linked list. Assume that index starts from 0. If the index is invalid, then you shouldn't perform the deletion.
+// After each query you need to print the linked list from both left to right and right to left.
 
-// Note: You must use Doubly Linked List, otherwise you will not get marks.
+// Note: You must use STL List, otherwise you will not get marks.
 
 // Input Format
 // First line will contain Q.
@@ -15,123 +16,64 @@ using namespace std;
 
 // Constraints
 // 1 <= Q <= 1000;
-// 0 <= X <= 1000;
-// 0 <= V <= 1000
+// 0 <= X <= 2;
+// 0 <= V <= 10^9
 
 // Output Format
-// For each query print the linked list from left to right and right to left or print "Invalid" as asked.
+// For each query print the linked list from left to right and right to left.
 // Print "L -> " before printing the linked list from left to right.
 // Print "R -> " before printing the linked list from right to left.
 
-class Node {
-
-    public:
-        int val;
-        Node* nextNode;
-        Node* prevNode;
-
-    Node(int val) {
-        this->val = val;
-        this->nextNode = NULL;
-        this->prevNode = NULL;
-    }
-
-};
-
-void buildLink(Node* prevIndexNode, Node* newNode, Node* &tail) {
-    if(prevIndexNode->nextNode!=NULL) {
-        newNode->nextNode = prevIndexNode->nextNode;
-        newNode->prevNode = prevIndexNode;
-        newNode->nextNode->prevNode = newNode;
-        prevIndexNode->nextNode = newNode;
-    }else {
-        prevIndexNode->nextNode = newNode;
-        newNode->prevNode = prevIndexNode;
-        tail=newNode;
+void printList(list<long long> myList) {
+    auto it = myList.begin();
+    for (it = myList.begin(); it != myList.end(); ++it) {
+        cout << *it << " ";
     }
 }
 
-void zigzagPrint(Node* head, Node* tail) {
-
-    Node* tmp = head;
+void zigzagPrint(list<long long> myList) {
+    
     cout << "L -> ";
-    for (tmp; tmp != NULL; tmp=tmp->nextNode)
-    {
-        cout << tmp->val << " ";
-    }
+    printList(myList);
     cout << endl;
-    
-    Node* tmp2 = tail;
+
     cout << "R -> ";
-    for (tmp2; tmp2 != NULL; tmp2=tmp2->prevNode)
-    {
-        cout << tmp2->val << " ";
-    }
+    list<long long> rList(myList);
+    rList.reverse();
+    printList(rList);
     cout << endl;
-    
+
 }
 
 int main ()
 {
 
-    Node* head = NULL;
-    Node* tail = NULL;
+    list<long long> list;
 
     int q;
     cin >> q;
 
-    while (q--)
-    {
-        int idx, val;
-        cin >> idx >> val;
+    int cmnd;
+    long long int val;
+    while (q--) {
+        cin >> cmnd >> val;
 
-        Node* newNode = new Node(val);
+        if(cmnd == 0) {
+            list.push_front(val);
 
-        if(idx == 0) {
-            if(head == NULL) {
-                head = newNode;
-                tail = newNode;
-            }else {
-                head->prevNode = newNode;
-                newNode->nextNode = head;
-                head = newNode;
-            }
-            zigzagPrint(head, tail);
-            continue;
-        }else {
-            if(head == NULL) {
-                cout << "Invalid" << endl;
+        } else if(cmnd == 1) {
+            list.push_back(val);
+
+        } else if (cmnd == 2) {
+            if (val >= list.size()) {
+                zigzagPrint(list);
                 continue;
             }
 
-            if(idx == 1) {
-                buildLink(head, newNode, tail);
-                zigzagPrint(head, tail);
-                continue;
-            }
-
-            int step = 0;
-            Node* tmp = head;
-            bool isInvalid = false;
-            while (step < idx-1)
-            {
-                if(tmp->nextNode == NULL) {
-                    cout << "Invalid" << endl;
-                    isInvalid = true;
-                    break;
-                }
-
-                tmp = tmp->nextNode;
-                step++;
-            }
-
-            if(isInvalid && tmp->nextNode == NULL) {
-                continue;
-            }
-            buildLink(tmp, newNode, tail);
-            zigzagPrint(head, tail);
+            list.erase(next(list.begin(), val));
         }
-        
+
+        zigzagPrint(list);
     }
 
     return 0;
