009ba427e2331c7f0a055d39a3f25f590c4b49d6
Sami-Shafi <freelancersami123@gmail.com>
2025-07-30 04:00:03 +0600

    Count Leaf Nodes

--- main.cpp
+++ main.cpp
@@ -66,9 +66,8 @@ void level_order(Node* root) {
 }
 
 int count_Nodes(Node* root) {
-    if (!root) {
+    if (!root)
         return 0;
-    }
 
     int l = count_Nodes(root->left);
     int r = count_Nodes(root->right);
@@ -76,10 +75,24 @@ int count_Nodes(Node* root) {
     return l + r + 1;
 }
 
+int count_Leaf_Nodes(Node* root) {
+    if (!root)
+        return 0;
+
+    if(!root->left && !root->right)
+        return 1;
+
+    int l = count_Leaf_Nodes(root->left);
+    int r = count_Leaf_Nodes(root->right);
+
+    return l + r;
+}
+
 int main ()
 {
     Node* root = input_tree();
     cout << count_Nodes(root) << endl;
+    cout << count_Leaf_Nodes(root) << endl;
     level_order(root);
     
     return 0;
