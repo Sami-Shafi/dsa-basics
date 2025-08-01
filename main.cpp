93a6d4838a55d1f124ce5a58cf38a8fae0480afa
Sami-Shafi <freelancersami123@gmail.com>
2025-08-01 13:50:50 +0600

    STL pair

--- main.cpp
+++ main.cpp
@@ -1,110 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-
-    public:
-        int val;
-        Node* left;
-        Node* right;
-
-    Node(int val) {
-        this->val = val;
-        this->left = NULL;
-        this->right = NULL;
-    }
-
-};
-
-Node* input_tree() {
-    int val;
-    cin >> val;
-    Node* root = new Node(val);
-    queue<Node *> q;
-    q.push(root);
-
-    while (!q.empty())
-    {
-        Node* f = q.front();
-        q.pop();
-
-        int l, r;
-        cin >> l >> r;
-        if(l != -1) {
-            f->left = new Node(l);
-            q.push(f->left);
-        }
-        if(r != -1) {
-            f->right = new Node(r);
-            q.push(f->right);
-        }
-    }
-    
-    return root;
-}
-
-void level_order(Node* root) {
-    if(!root) {
-        cout << "No Tree" << endl;
-        return;
-    }
-
-    queue<Node*> q;
-    q.push(root);
-
-    while (!q.empty())
-    {
-        Node* frontNode = q.front();
-        q.pop();
-        cout << frontNode->val << " ";
-        if(frontNode->left)
-            q.push(frontNode->left);
-        if(frontNode->right)
-            q.push(frontNode->right);
-    }
-    
-}
-
-int count_Nodes(Node* root) {
-    if (!root)
-        return 0;
-
-    int l = count_Nodes(root->left);
-    int r = count_Nodes(root->right);
-
-    return l + r + 1;
-}
-
-int count_Leaf_Nodes(Node* root) {
-    if (!root)
-        return 0;
-
-    if(!root->left && !root->right)
-        return 1;
-
-    int l = count_Leaf_Nodes(root->left);
-    int r = count_Leaf_Nodes(root->right);
-
-    return l + r;
-}
-
-int max_height(Node* root) {
-    if(!root)
-        return 0;
-    
-    if(!root->left && !root->right)
-        return 0;
-
-    int l = max_height(root->left);
-    int r = max_height(root->right);
-
-    return max(l,r) + 1;
-}
-
 int main ()
 {
-    Node* root = input_tree();
-    cout << max_height(root) << endl;
+    pair<string, int> p;
+    p = {"Sami", 20};
+    cout << p.first << endl;
     
     return 0;
 }
