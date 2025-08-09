ca77b98f72fe49c2278c7646a33b74d1afb95e69
Sami-Shafi <freelancersami123@gmail.com>
2025-08-10 03:46:15 +0600

    Priority queue with custom class and comparison

--- main.cpp
+++ main.cpp
@@ -1,30 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// You will be given a list A of size N. Then you will be given Q queries, for each query there will be some commands. Commands are given below -
+// You will be given a list A of size N. The list will contain data of some students where each student have Name, Roll and Marks. Then you will be given Q queries, for each query there will be some commands. Commands are given below -
 
-// 0 X -> Insert X into the list. Then print the current minimum value from the list.
-// 1 -> Print the current minimum value from the list.
-// 2 -> Delete the current minimum value from the list and print the minimum value from the list after deletion.
-// Note: If the list is empty and you can't print anything then you should print "Empty".
+// 0 X -> Insert  into the list where X is the data of a student which contain Name, Roll and Marks. Then print the current student's data who has the maximum Marks from the list.
+// 1 -> Print the current student's data who has the maximum Marks from the list.
+// 2 -> Delete the student who has the maximum Marks from the list and print the current student who has the maximum Marks from the list.
+// Note: If there are multiple students with same Marks then we will select whose Roll is smaller. If the list is empty and you can't print anything then you should print "Empty".
 
 // Input Format
 // First line will contain N.
-// Second line will contain the list A of size N.
+// Next N lines will contain Name, Role and Marks of student A.
 // Third line will contain Q.
 // Next Q lines will contain the commands.
+
+class Student {
+    public:
+        string name;
+        int mark;
+        int roll;
+    
+    Student(string name, int roll, int mark) {
+        this->name = name;
+        this->mark = mark;
+        this->roll = roll;
+    }
+};
+
+class custom {
+    public:
+        bool operator()(Student l, Student r) {
+            if(l.mark == r.mark)
+                return l.roll > r.roll;
+            return l.mark < r.mark;
+        }
+};
 
 int main ()
 {
     int size;
     cin >> size;
-    priority_queue<int, vector<int>, greater<int>> pq;
+    priority_queue<Student, vector<Student>, custom> pq;
 
     for (int i = 0; i < size; i++)
     {
-        int val;
-        cin >> val;
-        pq.push(val);
+        string name;
+        int mark, roll;
+        cin >> name >> roll >> mark;
+        pq.push(Student(name, roll, mark));
     }
 
     int T;
@@ -38,9 +61,10 @@ int main ()
         if(cmd != 0 && cmd != 1 && cmd != 2) continue;
 
         if(cmd == 0){
-            int x;
-            cin >> x;
-            pq.push(x);
+            string name;
+            int mark, roll;
+            cin >> name >> roll >> mark;
+            pq.push(Student(name, roll, mark));
         }
 
         if(cmd == 2){
@@ -52,8 +76,10 @@ int main ()
             }
         }
 
-        if(!pq.empty())
-            cout << pq.top() << endl;
+        if(!pq.empty()){
+            Student ourStudent = pq.top();
+            cout << ourStudent.name << " " << ourStudent.roll << " " << ourStudent.mark << endl;
+        }
         else
             cout << "Empty" << endl;
     }
