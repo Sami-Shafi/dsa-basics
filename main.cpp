604449e3614531666aca4ae431f7b77c0d8062e6
Sami-Shafi <freelancersami123@gmail.com>
2025-08-09 15:56:31 +0600

    Sorting with Set

--- main.cpp
+++ main.cpp
@@ -1,29 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// You will be given a list A of size N. You need to sort those values in ascending order and also you need to remove any duplicate values from the list and print the final outcome.
+
+// Input Format
+// First line will contain T, the number of test cases.
+// The first line of every test case will contain N.
+// The second line of every test case will contain the list A of size N.
+
 int main ()
 {
-    set<int> s;
+    int T;
+    cin >> T;
 
-    int n;
-    cin >> n;
-    while (n--)
+    while (T--)
     {
-        int val;
-        cin >> val;
-        s.insert(val);
-    }
+        int n;
+        cin >> n;
 
-    for (auto it = s.begin(); it!=s.end(); it++)
-    {
-        cout << *it << endl;
+        set<int> s;
+        for (int i = 0; i < n; i++)
+        {
+            int val;
+            cin >> val;
+            s.insert(val);
+        }
+
+        for (auto x : s)
+            cout << x << " ";
+
+        cout << endl;        
+        
     }
     
-    int target = 10;
-    if(s.count(target))
-        cout << target << " is present" << endl;
-    else
-        cout << target << " is not present" << endl;
         
     return 0;
 }
