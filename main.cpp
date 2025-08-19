fe93d4a3d7694a0dd8a48fb25a0cf2cf4f81576f
Sami-Shafi <freelancersami123@gmail.com>
2025-08-19 07:19:30 +0600

    Graph - BFS Traversal

--- main.cpp
+++ main.cpp
@@ -1,24 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> adj_list[1005];
+bool vis[1005];
+
+void bfs(int src) {
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+
+    while (!q.empty())
+    {
+        int f = q.front();
+        q.pop();
+
+        cout << f << " ";
+
+        for (int child : adj_list[f])
+        {
+            if(vis[child] != false)
+                continue;
+            
+            q.push(child);
+            vis[child] = true;
+        }
+        
+    }
+    
+}
+
 int main() {
     
     int n, e;
     cin >> n >> e;
-    vector<pair<int, int>> edge_list;
 
     for (int i = 0; i < e; i++)
     {
         int a, b;
         cin >> a >> b;
-        edge_list.push_back({a, b});
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
     }
 
-    for (auto p : edge_list)
-    {
-        cout << "{ " << p.first << ", " << p.second << " }" << endl;
-    }
-    
+    bfs(0);
     
     return 0;
 }
