861e7450b5fa25d1dca7733c94cf0722815c753d
Sami-Shafi <freelancersami123@gmail.com>
2025-08-19 06:54:49 +0600

    Graph - Adjacency List

--- main.cpp
+++ main.cpp
@@ -5,28 +5,23 @@ int main() {
     
     int n, e;
     cin >> n >> e;
-    int adj_mat[n][n];
-
-    memset(adj_mat, 0, sizeof(adj_mat));
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) 
-            if(i==j)
-                adj_mat[i][j] = 1;
+    vector<int> adj_list[n];
 
     for (int i = 0; i < e; i++)
     {
         int a, b;
-        cin >> a, b;
-        adj_mat[a][b] = 1;
-        // remove the line below for directed graph
-        adj_mat[b][a] = 1;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        // remove for directed graph
+        adj_list[b].push_back(a);
     }
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++) 
-            cout << adj_mat[i][j] << " ";
+        cout << i << " -> ";
+
+        for (int x : adj_list[i])
+            cout << x << " ";
         cout << endl;
     }
     
